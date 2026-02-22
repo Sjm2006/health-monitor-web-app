4761a02b8898d3d1af8eb165926b7b016d50de7e
Soumyajeet Mondal <soumyajitmondal511@gmail.com>
2026-02-22 15:50:37 +0530

    fix base url for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: '/health-monitor-web-app/',
   plugins: [react()],
   resolve: {
     alias: {
