c212d299b5d8860615c63d07a9eee50f0a004fc7
Soumyajeet Mondal <soumyajitmondal511@gmail.com>
2026-02-22 16:12:50 +0530

    fix routing for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const App = () => (
   <TooltipProvider>
     <Toaster />
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </TooltipProvider>
 );
 
