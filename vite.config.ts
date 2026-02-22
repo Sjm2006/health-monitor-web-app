9c7ced23b31228d3f3cc7dd1168ccfc97dd16282
Soumyajeet Mondal <soumyajitmondal511@gmail.com>
2026-02-22 16:03:03 +0530

    fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import path from "path";
 
 export default defineConfig({
   base: '/health-monitor-web-app/',
+  server: {
+    host: "::",
+    port: 8080,
+  },
   plugins: [react()],
   resolve: {
     alias: {
